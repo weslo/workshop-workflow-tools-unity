d10d5e2a494dc90f5e4d29c7717ca2042c8d1299
Weslo <wesley.rockholz@gmail.com>
2018-05-29 21:11:02 -0400

    Add scriptable object definitions

---
+++ Assets/Scripts/Data/CharacterDefinition.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+// Enumeration for a race of character.
+public enum CharacterRace {
+	Human,
+	Elf,
+	Dwarf,
+	Halfling,
+}
+
+// Enumeration for character classes.
+public enum CharacterClass {
+	Fighter,
+	Wizard,
+	Rogue,
+	Ranger,
+}
+
+// Data class that defines a character in our game.
+// Stored as a ScriptableObject in our assets folder.
+[CreateAssetMenu(fileName = "New Character", menuName = "Custom/Character Definition")]
+public class CharacterDefinition : ScriptableObject {
+	
+	// The maximum level a character can be.
+	public const int LEVEL_CAP = 20;
+
+	// This character's level.
+	[Range(1, 20)]
+	public int level;
+
+	// This character's race.
+	public CharacterRace race;
+
+	// This character's class.
+	public CharacterClass combatClass;
+
+	// The weapon this character wields.
+	private WeaponDefinition weapon;
+}
---
+++ Assets/Scripts/Data/WeaponDefinition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+// Enumeration for a class of weapon.
+public enum WeaponType {
+	Sword,
+	Axe,
+	Bow,
+}
+
+// Data class that defines a weapon in our game.
+// Stored as a ScriptableObject in our assets folder.
+public class WeaponDefinition : ScriptableObject {
+
+	// The amount of damage this weapon deals.
+	public int damage;
+
+	// The class this weapon.
+	public WeaponType weaponType;
+
+	// If true, this weapon takes two hands to weild.
+	public bool isTwoHanded;
+}
